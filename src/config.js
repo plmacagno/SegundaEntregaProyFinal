922eba374d6604fcb1c67d2e9767a29627d06a7c
Pablo Macagno <plmacagno@gmail.com>
2022-09-12 20:16:23 -0300

    seguridad

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
     path: "./data",
   },
   mongodb: {
-    cnxStr: "mongodb+srv://pablo:pablo1234@cluster0.p4ctutt.mongodb.net/?retryWrites=true&w=majority",
+    cnxStr: "mongodb+srv://user:password@cluster0.p4ctutt.mongodb.net/?retryWrites=true&w=majority",
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -11,15 +11,6 @@ export default {
     },
   },
   firebase: {
-    "type": "service_account",
-    "project_id": "ecommerce-33298",
-    "private_key_id": "d2d4e8f84ba15ba528e0806a7335f334c37c5bf3",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDbbxETx+FiV2Ac\nWXdNCW+cD6Bn4BtKRD/xeRnbXevxAN7ZcmHPHkJ9qjuE6fg4fUL1NFM9y1nbioVl\nQk05EvNDIBRwfUTOv2qP+aUAV5INdTwC2/Al5+cLZX7L+i/+WHRRHtFS+llgl8BH\ntVX0k9rtR/dtsCGVkCGmfCyuWZ0XUPO3rIKqkA9/CusCcjR115dAygewLSknEgoS\nmgcZlEhl2uUfGJ5kyjYFGTWiJIaebtAI/AsFPsmEw1Lr7F2yQAT6wJ6YnLc7HSFf\nRZfgRlClx4O9pME5GJjO96KubgSx2Lv1yxlo7Ck3lZF936MCkUGLqiQjkkO6wmeg\nrXc/gtwzAgMBAAECggEAV1lXCvC+H094ytAyVVA0RYNS7wae1mlkqtLyIAi7KyyV\niSmnFbON9EgXeUlJrfLu/HzfxfF0y5fQkgZahyWsAER83mp/QW2hFb7il8hEM2c/\nhEPYzL4LktXMK5ZkFHNRmhkSal2Nxo2h1OmyVbGlrF7Bd8BtHgRJ4OP9RUoW5nhu\nVmFIUVEYgn4BoUB/ky6pG69geqiwzUC1z/NoTt+e/+3H0m8MmfYVbrO0rRB4CSJm\n4GmDjO28h/AVZP9aqG4wF5qOxWl4HsEDc3rqxEGeSdrEBTK3+/cHQPLr+AXXP0Le\n6XIK/xSBIbGe5x5u/etnppMg0VvBEpho6uuVpLeXAQKBgQDv0kniLXU0IXGGT0hP\n2boYMsnbU9cJzrusWqpDOK8focR3Ti+e9kseEsDFMkVWSqDUQZ7/VkZ95IVW0bz+\nBpsHrPnRJ3oRTl/P/SJmPTnLYUkjgG1vbNowtqeUtiyJFDRyqn8tOn+9ajGIsep6\neyIl0Lf8Uqr11bayhAe6tklAowKBgQDqPK9WbzInjq2vcUc+xjXeadhUxSCMkEuo\n1wMTEPCkQaV2/7WWYk1UCI0A4D0HaUppV5ITPC9j/B/dl9KV/z4Q84zN6cr74kmV\nAmESZo7j1ImLVoGwpJvzhHgfIhSWBXsrGz44KWd5KmPzM3ORSd0dmExwx8CUA7nf\nUGph3ItfMQKBgGR5ZMzmoKDrlgVgN/qETIwQ0zMZiBEq1MPb5FNvbydAffBCCH9/\nzm7WCn9TlWsWrxTePT/apIPf9uEt1BFVMSRhGvS+pkyT2l5oPj45Tnpf0uWVy8OQ\nBkW8cK9EN5pipaqYtdl8FcKEuCX97C/OYLsZG8wR0mBWXkmiOR7Heh8jAoGBAMob\nEGKw0QKmriAZypJUbbh5dUo3SI1gF5gX5TQ3QptHdSCHJEQcj4I5Woht8tqJM8ZO\n4+Aq5u0LZudJD0+TCxePPWeSGS1ao4ipo94/lXzpLsCGJKKodVMZcpfSTRbgpdSc\nzgOhXgGC3PjD5SxwIhReC16oS/aqfF89wHyt0aeRAoGANIyWmw5bpkFldy9ezwLL\ntnejlJmeOklfV1VN/hWj1nxSIWMvrbjMRNolMhzmZ4s1IEbl5X5fo/VJp34EHL1c\nrnrA8Mye9SPQMhrbB8eR/fMK4RLx/ElhuMLEMN0JYyJktMqD7cKP3MrnXar8whZE\nDq2xLYjkkzL+tEo5enZedQo=\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-p84g6@ecommerce-33298.iam.gserviceaccount.com",
-    "client_id": "111667294387612311150",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-p84g6%40ecommerce-33298.iam.gserviceaccount.com"
+    
     },
 };
